e479d903aba28340c74ecc3748d5a3d742f88816
tixlag <113098223+tixlag@users.noreply.github.com>
2023-04-05 01:12:32 +0800

    Delete src directory

--- src/main/java/ru/kata/spring/boot_security/demo/util/UserExistException.java
+++
@@ -1,7 +0,0 @@
-package ru.kata.spring.boot_security.demo.util;
-
-public class UserExistException  extends RuntimeException {
-    public UserExistException(String message) {
-        super(message);
-    }
-}
